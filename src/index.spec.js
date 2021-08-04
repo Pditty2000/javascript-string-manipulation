491633365e85954f1249c1c3f24a23fe5cfce51d
Johnston, Peter <peter.johnston@wizards.com>
2021-08-04 15:33:44 -0700

    Completed module

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-const { assert } require("chai");
+const { assert } = require("chai");
 const { describe, it } = require("mocha");
 const { lf } = global.lf || require("index.js");
 
@@ -8,7 +8,7 @@ describe("CRLF Converter", () => {
     it("should replace CRLF at the beginning of a string", () => {
       const result = lf`\r\nHello there.`;
 
-      assert(result.startsWith("\nHello there"));
+      assert(result.startsWith("\nHello"));
     });
 
     it("should replace CRLF at the end of a string", () => {
@@ -20,7 +20,7 @@ describe("CRLF Converter", () => {
     it("should replace CRLF in the middle of a string", () => {
       const result = lf`Hello \r\n\r\nthere.`;
 
-      assert(result.includes("o \n\nT"));
+      assert(result.includes("o \n\nt"));
     });
   });
 });
